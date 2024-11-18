8f5b290cc3f03379a4f687dd787e36c80cabcd5b
peterxiewx <1459454060@qq.com>
2024-11-18 18:09:45 +0800

    Add files via upload
    greeting

---
+++ php/Src/MyGreeter.php
@@ -0,0 +1,51 @@
+<?php
+namespace Src;
+
+class MyGreeter {
+
+    private $exec_time = null;
+
+    public function __construct()
+    {
+        $this->exec_time = date("H");
+    }
+
+    /**
+     * 根据当前时间确定问候语
+     */
+    public function greeting() : string {
+        /**
+         * Between 6AM and 12AM
+         */
+        if ($this->exec_time >= 6 && $this->exec_time < 12) {
+            return $this->morning();
+        }
+        /**
+         * Between 12AM and 6PM
+         */
+        if ($this->exec_time >= 12 && $this->exec_time < 18) {
+            return $this->afternoon();
+        }
+        /**
+         * Between 6PM and 6AM
+         */
+        if (($this->exec_time >= 18 && $this->exec_time < 24) 
+        || ($this->exec_time >= 0 && $this->exec_time < 6)) {
+            return $this->evening();
+        }
+        
+        return "";
+    }
+
+    private function morning() : string {
+        return "Good morning";
+    }
+
+    private function afternoon() : string {
+        return "Good afternoon";
+    }
+
+    private function evening() : string {
+        return "Good evening";
+    }
+}
